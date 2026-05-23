19cc39947efb23d5212634b6b073009dbb953036
chulingera2025 <chulingera2025@gmail.com>
2026-05-23 11:29:56 +0800

    feat: keep only copy interception, remove server hooks

--- library.js
+++ library.js
@@ -1,43 +1,5 @@
 'use strict';
 
-const url = require('url');
-
 const plugin = {};
-
-plugin.filterMetaTags = async (data) => {
-    if (!Array.isArray(data.tags)) {
-        data.tags = [];
-    }
-
-    const parsed = url.parse(data.req.originalUrl || '');
-
-    let cleanPath = parsed.pathname || '/';
-
-    cleanPath = cleanPath
-        .replace(/^\/topic\/(\d+)\/[^/?#]+(\/.*)?$/, '/topic/$1$2')
-        .replace(/^\/category\/(\d+)\/[^/?#]+(\/.*)?$/, '/category/$1$2');
-
-    const canonicalUrl =
-        `${data.req.protocol}://${data.req.get('host')}${cleanPath}`;
-
-    data.tags = data.tags.filter(tag => {
-        return !(
-            tag &&
-            tag.tagName === 'link' &&
-            tag.attributes &&
-            tag.attributes.rel === 'canonical'
-        );
-    });
-
-    data.tags.push({
-        tagName: 'link',
-        attributes: {
-            rel: 'canonical',
-            href: canonicalUrl,
-        },
-    });
-
-    return data;
-};
 
 module.exports = plugin;
